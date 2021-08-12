fcb3233a9e088ce311a6bf01981b1d94902872da
Vadim <Vadim@DESKTOP-CFEBN1P>
2021-08-12 23:05:55 +0500

    feat: init project

---
+++ src/angular-i18n-demo-schematics/index_spec.ts
@@ -0,0 +1,16 @@
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('angular-i18n-demo-schematics', () => {
+  it('works', async () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner
+      .runSchematicAsync('angular-i18n-demo-schematics', {}, Tree.empty())
+      .toPromise();
+
+    expect(tree.files).toEqual([]);
+  });
+});
